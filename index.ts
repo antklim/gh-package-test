d8304cd42a1d631c66714c6733fa0badd97b70ff
antklim <antklim@gmail.com>
2023-06-01 16:03:00 +1000

    feat: added sqr operation

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ export const sub = (a: number, b: number) => a - b
 export const mul = (a: number, b: number) => a * b
 
 export const div = (a: number, b: number) => a / b
+
+export const sqr = (a: number) => mul(a, a)
