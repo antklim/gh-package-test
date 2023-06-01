1c96b3ab0906160df53bde4046a28936126cbf83
antklim <antklim@gmail.com>
2023-06-01 16:29:32 +1000

    feat: added sqrt operation

--- index.ts
+++ index.ts
@@ -7,3 +7,5 @@ export const mul = (a: number, b: number) => a * b
 export const div = (a: number, b: number) => a / b
 
 export const sqr = (a: number) => mul(a, a)
+
+export const sqrt = (a: number) => Math.sqrt(a)
