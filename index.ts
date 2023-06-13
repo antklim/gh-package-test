48d762cf00c21315ca205de36ecb2c501951b12a
antklim <antklim@gmail.com>
2023-06-13 10:38:46 +1000

    feat: added abs method

--- index.ts
+++ index.ts
@@ -9,3 +9,5 @@ export const div = (a: number, b: number) => a / b
 export const sqr = (a: number) => mul(a, a)
 
 export const sqrt = (a: number) => Math.sqrt(a)
+
+export const abs = (a: number) => Math.abs(a)
